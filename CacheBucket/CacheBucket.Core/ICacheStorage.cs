539e5dbb7ae63b699fe8533e043fc5b48ff981f1
Jkaveri <haiho1992@gmail.com>
2017-10-05 01:03:25 +0700

    Add cache bucket factory to help create cache bucket instance by name. (#2)

--- CacheBucket/CacheBucket.Core/ICacheStorage.cs
+++ CacheBucket/CacheBucket.Core/ICacheStorage.cs
@@ -3,7 +3,7 @@
     public interface ICacheStorage
     {
         string Get(string key);
-        void Set(string key, string value);
         void Remove(string key);
+        void Set(string key, string value);
     }
 }
---
+++ CacheBucket/CacheBucket.Factory/CacheStorageMapping.cs
@@ -0,0 +1,47 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace CB.Factory
+{
+    internal class CacheStorageMapping
+    {
+        private static readonly Lazy<CacheStorageMapping> LazyInstance = new Lazy<CacheStorageMapping>();
+        private readonly Dictionary<string, Type> _map = new Dictionary<string, Type>();
+
+        public static CacheStorageMapping Instance => LazyInstance.Value;
+
+        public void AddMapping(string bucketName, Type type)
+        {
+            if (_map.ContainsKey(bucketName))
+            {
+                _map[bucketName] = type;
+            }
+            else
+            {
+                _map.Add(bucketName, type);
+            }
+        }
+
+        public void RemoveMapping(string bucketName)
+        {
+            if (_map.ContainsKey(bucketName))
+            {
+                _map.Remove(bucketName);
+            }
+        }
+
+        public Type GetStorageType(string bucketName)
+        {
+            if (_map.ContainsKey(bucketName))
+            {
+                return _map[bucketName];
+            }
+
+            throw new InvalidOperationException("Mapping doesn't exist.");
+        }
+    }
+}
