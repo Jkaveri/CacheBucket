53d1c21b098d1e4746de8e7c2e9f32b5d77d4d95
Henry Nguyen <haiho1992@gmail.com>
2017-10-12 17:47:22 +0700

    Add Get helper.

---
+++ CacheBucket/src/CacheBucket.Core/ICacheValueConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CB.Core
+{
+    public interface ICacheValueConverter
+    {
+        /// <summary>
+        /// Convert string to type.
+        /// </summary>
+        T To<T>(string input) where T: struct;
+    }
+}
