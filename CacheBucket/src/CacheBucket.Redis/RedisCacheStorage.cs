2d68cc548cb4ec9ae57e152f94edebf1323b26c6
Henry Nguyen <haiho1992@gmail.com>
2017-10-12 17:46:53 +0700

    Add redis cache storage.

---
+++ CacheBucket/src/CacheBucket.Redis/RedisCacheStorage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CB.Core;
+using ServiceStack.Redis;
+
+namespace CacheBucket.Redis
+{
+    public class RedisCacheStorage : ICacheStorage
+    {
+        private readonly IRedisClientsManager _clientManager;
+
+        public RedisCacheStorage(IRedisClientsManager clientManager)
+        {
+            if (clientManager == null)
+            {
+                throw new ArgumentNullException(nameof(clientManager));
+            }
+
+            _clientManager = clientManager;
+        }
+        public string Get(string key)
+        {
+            using (IRedisClient client = _clientManager.GetClient())
+            {
+                return client.GetValue(key);
+            }
+        }
+
+        public void Remove(string key)
+        {
+            using (IRedisClient client = _clientManager.GetClient())
+            {
+                client.Remove(key);
+            }
+        }
+
+        public void Set(string key, string value)
+        {
+            using (IRedisClient client = _clientManager.GetClient())
+            {
+                client.SetValue(key, value);
+            }
+        }
+    }
+}
